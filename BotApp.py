964c6c8a5fd4c68ca29556d9c973829b9254d544
buchynskiy <pbuchinskiy@gmail.com>
2023-07-28 12:08:21 +0200

    Funished bot code

--- BotApp.py
+++ BotApp.py
@@ -34,6 +34,23 @@ def enough(update: Update, context: CallbackContext) -> None:
     context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
     update.message.reply_text('If you want to stop receiving motivation from the bot --> just delete the chat with the bot.')
 
+def getQuote(context: CallbackContext):
+    get_quote = random.choices(quotes)
+    clear_quote = ', '.join(map(str, get_quote))
+    # code below sends a random quote to the subscribed users. 
+    context.bot.send_message(chat_id=000000000, text=clear_quote)
+    context.bot.send_message(chat_id=000000000, text=clear_quote)
+    context.bot.send_message(chat_id=000000000, text=clear_quote)
+    context.bot.send_message(chat_id=000000000, text=clear_quote)
+
+def motivation(update: Update, context: CallbackContext):
+    context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
+    user_username = update._effective_user.username
+    user_id = update._effective_user.id
+    # here the chat_id of the bot administrator should be specified
+    context.bot.send_message(chat_id=000000000, text=f'\U00002757 New user subscribed:\n\nUsername: @{user_username}\nUserID: {user_id}')
+    update.message.reply_text("From now and on you will be receiving motivation twice a day:\n\n\U00002022 7:30 AM UTC\n\U00002022 5:00 PM UTC\n\nHave a nice day \U0001F643")
+
 def echo(update: Update, context: CallbackContext) -> None:
     default_message = 'I am just a bot. Sometimes there are things I don\'t understand \U0001F916'
     update.message.reply_text(default_message, reply_to_message_id=update._effective_message.message_id)
@@ -71,10 +88,15 @@ def main() -> None:
     dp = updater.dispatcher
 
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dp.add_handler(CommandHandler('motivation', motivation))
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('info', info))
     dp.add_handler(CommandHandler('enough', enough))
     dp.add_handler(CallbackQueryHandler(button))
+
+    j = dp.job_queue
+    j.run_daily(callback=getQuote, time=datetime.time(hour=7, minute=30, second=00), days=(0, 1, 2, 3, 4, 5, 6))
+    j.run_daily(callback=getQuote, time=datetime.time(hour=17, minute=00, second=00), days=(0, 1, 2, 3, 4, 5, 6))
 
     updater.start_polling()
     updater.idle()
